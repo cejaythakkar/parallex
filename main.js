eba4986c790f4948fa1cc4bd60f068db547ca093
cejaythakkar <ce.jaythakkar@gmail.com>
2015-09-03 20:11:52 +0530

    k

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var express = require('express'),
 	app = express(),
 	port = process.env.PORT || 3000;
 
-express.use(express.static(__dirname + '/public'));
+express.use(express.static('public'));
 
 express.listen(port,function(){
 	console.log('server up and running ....!');
